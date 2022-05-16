1053b45ce133c30aace5ab1a3afada4c69f7be27
Mateo <mateo.munoz@masivian.com>
2022-05-16 15:27:35 -0500

    Rating and favorites comics component

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,16 +1,55 @@
 <template>
   <div>
     <main-layout>
-      <div>Hola</div>
+      <div class="container favorites-comics">
+        <h1 class="title has-text-white is-1">Favorites</h1>
+        <div v-for="(item, key) in favoritesComics" :key="key" class="card">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image">
+                  <img :src="item.img" alt="Comic Image">
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-4">{{ item.title }} ({{ item. num }})</p>
+                <p class="subtitle is-6">{{ item.alt }}</p>
+                <rating
+                  :grade="item.rating"
+                  @change="rating(key, item.rating)"
+                  v-model="item.rating"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </main-layout>
   </div>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex';
+import { typesComic as types } from '@/store/modules/comics/types';
+
 import MainLayout from '../components/MainLayout.vue';
+import Rating from '../components/Rating.vue';
 
 export default {
   name: 'Favorites',
-  components: { MainLayout },
+  components: { MainLayout, Rating },
+  computed: {
+    ...mapState(types.PATH, [
+      'favoritesComics',
+    ]),
+  },
+  methods: {
+    ...mapActions(types.PATH, {
+      addComicRating: types.actions.ADD_COMIC_RATING,
+    }),
+    rating(key, model) {
+      this.addComicRating({ key, rate: model });
+    },
+  },
 };
 </script>
